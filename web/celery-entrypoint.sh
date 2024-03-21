9b8e8d367efa4c5324c1d617b6ef12bee13a1387
David B <44925796+TH3xACE@users.noreply.github.com>
2024-03-21 22:22:23 +0100

    Update celery-entrypoint.sh

--- web/celery-entrypoint.sh
+++ web/celery-entrypoint.sh
@@ -162,7 +162,7 @@ if [ "$DEBUG" == "1" ]; then
 fi
 
 # Load Custom Scan Engines
-#python3 manage.py loadcustomengines
+python3 manage.py loadcustomengines
 
 # watchmedo auto-restart --recursive --pattern="*.py" --directory="/usr/src/app/reNgine/" -- celery -A reNgine.tasks worker --autoscale=10,0 -l INFO -Q scan_queue &
 echo "Starting Workers..."
